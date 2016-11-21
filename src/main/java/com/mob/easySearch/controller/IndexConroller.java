e327660e2baad9fbd44136cea757d92bd565ea7d
zxc337 <zhangxiongcai337@163.com>
2016-11-21 12:22:28 +0800

    .

--- src/main/java/com/mob/easySearch/controller/IndexConroller.java
+++ src/main/java/com/mob/easySearch/controller/IndexConroller.java
@@ -37,7 +37,7 @@ public class IndexConroller extends BaseController {
     @PathVariable("indexType")
     final String indexType, @RequestBody Object... data) {
         if (StringUtils.isEmpty(indexName) || StringUtils.isEmpty(indexType)) return fail("参数错误");
-        if (data == null || data.length == 0) return fail("文档文库");
+        if (data == null || data.length == 0) return fail("文档为空");
         if (!es.existsIndex(indexName)) return fail("索引未定义");
 
         for (Object source : data) {
